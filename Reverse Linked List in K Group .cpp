3f34a9438195ba4e52bb3a98c7507c1456826b8d
Parth <94388365+praxton74@users.noreply.github.com>
2023-02-01 01:44:59 +0530

    Placement

---
+++ Reverse Linked List in K Group .cpp
@@ -0,0 +1,27 @@
+class Solution
+{
+    public:
+    struct node *reverse (struct node *head, int k)
+    { 
+        // Complete this method
+        
+        node*prev=NULL;
+        node*curr=head;
+        node*fut;
+        int c=1;
+        while(curr!=NULL && c<=k)
+        {       
+            fut=curr->next;
+            curr->next=prev;
+            prev=curr;;
+            curr=fut;
+            c++;
+        }
+        if(fut!=NULL)
+        {
+            head->next=reverse (fut,k);
+        }
+        
+        return prev;
+    }
+};
