199ed69e8f38cdd5c7a8aa9f8123731dc0906d87
Parth <94388365+praxton74@users.noreply.github.com>
2023-02-01 01:15:44 +0530

    **

---
+++ Reverse Linked List .cpp
@@ -0,0 +1,23 @@
+class Solution
+{
+    public:
+    //Function to reverse a linked list.
+    struct Node* reverseList(struct Node *head)
+    {
+        // code here
+        // return head of reversed list
+        Node* prev=NULL;
+        Node*curr=head;
+        Node*fut=head->next;
+        while(curr!=NULL)
+        {
+            fut=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=fut;
+        }
+        return prev;
+        
+    }
+    
+};
