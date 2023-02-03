41245652f300b72add1dc91cc5480ece04744b22
Parth <94388365+praxton74@users.noreply.github.com>
2023-02-04 01:14:26 +0530

    Important Approach

---
+++ Intersection of Two Linked Lists .cpp
@@ -0,0 +1,19 @@
+int intersectPoint(Node* head1, Node* head2)
+{
+    // Your Code Here
+    
+    if(head1==NULL)
+    return NULL;
+    else if(head2==NULL) 
+    return NULL;
+    Node*p1,*p2;
+    p1=head1;
+    p2=head2;
+    
+    while(p1!=p2)
+    {
+        p1=(p1==NULL)?head2:p1->next;
+        p2=(p2==NULL)?head1:p2->next;
+    }
+    return p1->data;
+}
