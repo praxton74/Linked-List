ade2eb4d948af13814415a04fc69b8a82d667a04
Parth <94388365+praxton74@users.noreply.github.com>
2023-02-25 02:07:03 +0530

    Create Sort 0&1 List .cpp

---
+++ Sort 0&1 List .cpp
@@ -0,0 +1,29 @@
+ListNode* Solution::solve(ListNode* A)
+{
+    int zerocount =0;
+    int onecount=0;
+    ListNode* temp = A;
+   
+    while(temp!=NULL){
+        if(temp->val == 0){
+            zerocount++;
+        }
+        if(temp->val == 1){
+            onecount++;
+        }
+        temp = temp->next ;
+    }
+    ListNode* temp1 = A;
+    while(temp1!=NULL){
+        if(zerocount != 0){
+            temp1->val = 0;
+            zerocount--;
+        }
+        else if(onecount !=0){
+            temp1->val =1;
+            onecount--;
+        }
+        temp1 = temp1->next;
+    }
+    return A;
+}
